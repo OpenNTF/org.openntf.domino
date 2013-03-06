3faac5204fe204fd96743444842b596be63343b3
Jesse Gallagher <jesse@secondfoundation.org>
2013-03-06 09:02:42 -0500

    Added AdministrationProcess

---
+++ org.openntf.domino/src/org/openntf/domino/AdministrationProcess.java
@@ -0,0 +1,6 @@
+package org.openntf.domino;
+
+public interface AdministrationProcess extends lotus.domino.AdministrationProcess,
+		org.openntf.domino.Base<lotus.domino.AdministrationProcess> {
+
+}
