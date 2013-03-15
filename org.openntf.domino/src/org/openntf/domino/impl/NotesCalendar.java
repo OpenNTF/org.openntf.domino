a3d5f658c1b968d1cb4cce21377435bf4b157f8d
Jesse Gallagher <jesse@secondfoundation.org>
2013-03-15 14:08:22 -0400

    Added implementations and interfaces for NotesCalendar classes

---
+++ org.openntf.domino/src/org/openntf/domino/impl/NotesCalendar.java
@@ -0,0 +1,237 @@
+package org.openntf.domino.impl;
+
+import java.util.Vector;
+
+import lotus.domino.NotesException;
+
+import org.openntf.domino.utils.DominoUtils;
+import org.openntf.domino.utils.Factory;
+
+public class NotesCalendar extends Base<org.openntf.domino.NotesCalendar, lotus.domino.NotesCalendar> implements
+		org.openntf.domino.NotesCalendar {
+
+	public NotesCalendar(lotus.domino.NotesCalendar delegate, org.openntf.domino.Base<?> parent) {
+		super(delegate, parent);
+	}
+
+	@Override
+	public NotesCalendarEntry createEntry(String iCalEntry) {
+		try {
+			return Factory.fromLotus(getDelegate().createEntry(iCalEntry), NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public NotesCalendarEntry createEntry(String iCalEntry, int flags) {
+		try {
+			return Factory.fromLotus(getDelegate().createEntry(iCalEntry, flags), NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public boolean getAutoSendNotices() {
+		try {
+			return getDelegate().getAutoSendNotices();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return false;
+		}
+	}
+
+	@Override
+	public Vector<org.openntf.domino.NotesCalendarEntry> getEntries(lotus.domino.DateTime start, lotus.domino.DateTime end) {
+		try {
+			return Factory.fromLotusAsVector(getDelegate().getEntries((lotus.domino.DateTime) toLotus(start),
+					(lotus.domino.DateTime) toLotus(end)), org.openntf.domino.NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public Vector<org.openntf.domino.NotesCalendarEntry> getEntries(lotus.domino.DateTime start, lotus.domino.DateTime end, int skipCount,
+			int maxReturn) {
+		try {
+			return Factory.fromLotusAsVector(getDelegate().getEntries((lotus.domino.DateTime) toLotus(start),
+					(lotus.domino.DateTime) toLotus(end), skipCount, maxReturn), org.openntf.domino.NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public int getEntriesProcessed() {
+		try {
+			return getDelegate().getEntriesProcessed();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return 0;
+		}
+	}
+
+	@Override
+	public NotesCalendarEntry getEntry(String uid) {
+		try {
+			return Factory.fromLotus(getDelegate().getEntry(uid), NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public NotesCalendarEntry getEntryByNoteID(String noteid) {
+		try {
+			return Factory.fromLotus(getDelegate().getEntryByNoteID(noteid), NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public NotesCalendarEntry getEntryByUNID(String unid) {
+		try {
+			return Factory.fromLotus(getDelegate().getEntryByUNID(unid), NotesCalendarEntry.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public Vector<org.openntf.domino.NotesCalendarNotice> getNewInvitations() {
+		try {
+			return Factory.fromLotusAsVector(getDelegate().getNewInvitations(), NotesCalendarNotice.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public Vector<org.openntf.domino.NotesCalendarNotice> getNewInvitations(lotus.domino.DateTime start, lotus.domino.DateTime since) {
+		try {
+			return Factory.fromLotusAsVector(getDelegate().getNewInvitations((lotus.domino.DateTime) toLotus(start),
+					(lotus.domino.DateTime) toLotus(since)), NotesCalendarNotice.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public NotesCalendarNotice getNoticeByUNID(String unid) {
+		try {
+			return Factory.fromLotus(getDelegate().getNoticeByUNID(unid), NotesCalendarNotice.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public int getReadRangeMask1() {
+		try {
+			return getDelegate().getReadRangeMask1();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return 0;
+		}
+	}
+
+	@Override
+	public int getReadRangeMask2() {
+		try {
+			return getDelegate().getReadRangeMask2();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return 0;
+		}
+	}
+
+	@Override
+	public int getReadXLotusPropsOutputLevel() {
+		try {
+			return getDelegate().getReadXLotusPropsOutputLevel();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return 0;
+		}
+	}
+
+	@Override
+	public org.openntf.domino.DateTime getUntilTime() {
+		try {
+			return Factory.fromLotus(getDelegate().getUntilTime(), DateTime.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public String readRange(lotus.domino.DateTime start, lotus.domino.DateTime end) {
+		try {
+			return getDelegate().readRange((lotus.domino.DateTime) toLotus(start), (lotus.domino.DateTime) toLotus(end));
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public String readRange(lotus.domino.DateTime start, lotus.domino.DateTime end, int skipCount, int maxRead) {
+		try {
+			return getDelegate()
+					.readRange((lotus.domino.DateTime) toLotus(start), (lotus.domino.DateTime) toLotus(end), skipCount, maxRead);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public void setAutoSendNotices(boolean flag) {
+		try {
+			getDelegate().setAutoSendNotices(flag);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void setReadRangeMask1(int mask) {
+		try {
+			getDelegate().setReadRangeMask1(mask);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void setReadRangeMask2(int mask) {
+		try {
+			getDelegate().setReadRangeMask2(mask);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void setReadXLotusPropsOutputLevel(int level) {
+		try {
+			getDelegate().setReadXLotusPropsOutputLevel(level);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+}
---
+++ org.openntf.domino/src/org/openntf/domino/impl/NotesCalendarNotice.java
@@ -0,0 +1,183 @@
+package org.openntf.domino.impl;
+
+import java.util.Vector;
+
+import lotus.domino.NotesException;
+
+import org.openntf.domino.utils.DominoUtils;
+import org.openntf.domino.utils.Factory;
+
+public class NotesCalendarNotice extends Base<org.openntf.domino.NotesCalendarNotice, lotus.domino.NotesCalendarNotice> implements
+		org.openntf.domino.NotesCalendarNotice {
+
+	public NotesCalendarNotice(lotus.domino.NotesCalendarNotice delegate, org.openntf.domino.Base<?> parent) {
+		super(delegate, parent);
+	}
+
+	@Override
+	public void accept(String comments) {
+		try {
+			getDelegate().accept(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void acceptCounter(String comments) {
+		try {
+			getDelegate().acceptCounter(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void counter(String comments, lotus.domino.DateTime start, lotus.domino.DateTime end) {
+		try {
+			getDelegate().counter(comments, (lotus.domino.DateTime) toLotus(start), (lotus.domino.DateTime) toLotus(end));
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void counter(String comments, lotus.domino.DateTime start, lotus.domino.DateTime end, boolean keepPlaceholder) {
+		try {
+			getDelegate().counter(comments, (lotus.domino.DateTime) toLotus(start), (lotus.domino.DateTime) toLotus(end), keepPlaceholder);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void decline(String comments) {
+		try {
+			getDelegate().decline(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void decline(String comments, boolean keepInformed) {
+		try {
+			getDelegate().decline(comments, keepInformed);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void declineCounter(String comments) {
+		try {
+			getDelegate().declineCounter(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void delegate(String commentsToOrganizer, String delegateTo) {
+		try {
+			getDelegate().delegate(commentsToOrganizer, delegateTo);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void delegate(String commentsToOrganizer, String delegateTo, boolean keepInformed) {
+		try {
+			getDelegate().delegate(commentsToOrganizer, delegateTo, keepInformed);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public Document getAsDocument() {
+		try {
+			return Factory.fromLotus(getDelegate().getAsDocument(), Document.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public String getNoteID() {
+		try {
+			return getDelegate().getNoteID();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public Vector<org.openntf.domino.NotesCalendarNotice> getOutstandingInvitations() {
+		try {
+			return Factory.fromLotusAsVector(getDelegate().getOutstandingInvitations(), org.openntf.domino.NotesCalendarNotice.class, this);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public String getUNID() {
+		try {
+			return getDelegate().getUNID();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public String read() {
+		try {
+			return getDelegate().read();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
+
+	@Override
+	public void removeCancelled() {
+		try {
+			getDelegate().removeCancelled();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void requestInfo(String comments) {
+		try {
+			getDelegate().requestInfo(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void sendUpdatedInfo(String comments) {
+		try {
+			getDelegate().sendUpdatedInfo(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	@Override
+	public void tentativelyAccept(String comments) {
+		try {
+			getDelegate().tentativelyAccept(comments);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+}
