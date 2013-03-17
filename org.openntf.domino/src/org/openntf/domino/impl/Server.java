c2ffcad301a2e7b72bc56d2475e4fc469d284acc
muenzpraeger <mail@winkelmeyer.com>
2013-03-17 16:38:50 +0100

    Added first round of method implementations for Server class

--- org.openntf.domino/src/org/openntf/domino/impl/Server.java
+++ org.openntf.domino/src/org/openntf/domino/impl/Server.java
@@ -18,221 +18,498 @@ package org.openntf.domino.impl;
 import java.util.Collection;
 import java.util.List;
 
+import lotus.domino.NotesException;
+import lotus.notes.addins.DominoServer;
 import lotus.notes.addins.ServerAccess;
 import lotus.notes.addins.ServerInfo;
 
 import org.openntf.domino.Database;
+import org.openntf.domino.utils.DominoUtils;
 
 public class Server implements org.openntf.domino.Server {
 
+	private DominoServer server_;
+
+	public Server(String serverName) {
+		try {
+			server_ = new DominoServer(serverName);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#checkServerAccess(java.lang.String, java.util.Collection)
+	 */
 	@Override
 	public boolean checkServerAccess(String userName, Collection<ServerAccess> serverAccessLevel) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			return server_.checkServerAccess(userName, serverAccessLevel);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return false;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#checkServerAccess(java.lang.String, lotus.notes.addins.ServerAccess)
+	 */
 	@Override
 	public boolean checkServerAccess(String userName, ServerAccess serverAccessLevel) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			return server_.checkServerAccess(userName, serverAccessLevel);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return false;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getAbbreviatedName()
+	 */
 	@Override
 	public String getAbbreviatedName() {
-		// TODO Auto-generated method stub
-		return null;
+		return server_.getAbbreviatedName();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getCanonicalName()
+	 */
 	@Override
 	public String getCanonicalName() {
-		// TODO Auto-generated method stub
-		return null;
+		return server_.getCanonicalName();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getClusterMembers(boolean)
+	 */
 	@Override
 	public Collection<org.openntf.domino.Server> getClusterMembers(boolean includeCurrentServer) {
-		// TODO Auto-generated method stub
+		// TODO: Add method to read all cluster members
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getClusterName()
+	 */
 	@Override
 	public String getClusterName() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getCluster();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getDatabaseByFilePath(java.lang.String)
+	 */
 	@Override
 	public Database getDatabaseByFilePath(String filePath) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getDatabaseByFilePath(java.lang.String, boolean)
+	 */
 	@Override
 	public Database getDatabaseByFilePath(String filePath, boolean failOver) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getDatabaseByReplicaId(java.lang.String)
+	 */
 	@Override
 	public Database getDatabaseByReplicaId(String replicaId) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getDatabaseByReplicaId(java.lang.String, boolean)
+	 */
 	@Override
 	public Database getDatabaseByReplicaId(String replicaId, boolean failOver) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getDomain()
+	 */
 	@Override
 	public String getDomain() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getDomain();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getPlatform()
+	 */
 	@Override
 	public String getPlatform() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getPlatform();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getServerDocField(java.lang.String)
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<String> getServerDocField(String fieldName) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.lookupServerRecord(fieldName);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getServerDocField(java.lang.String, java.lang.String)
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<String> getServerDocField(String fieldName, String unknown) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.lookupServerRecord(fieldName, unknown);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getServerInfo(lotus.notes.addins.ServerInfo)
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<String> getServerInfo(ServerInfo serverInfo) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getInfo(serverInfo);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getTitle()
+	 */
 	@Override
 	public String getTitle() {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getTitle();
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getUserNamesList(java.util.Collection)
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<String> getUserNamesList(Collection<String> userNames) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getNamesList(userNames);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getUserNamesList(java.lang.String)
+	 */
+	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<String> getUserNamesList(String userName) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.getNamesList(userName);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#getVersion()
+	 */
 	@Override
 	public String getVersion() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#isCluster()
+	 */
 	@Override
 	public boolean isCluster() {
-		// TODO Auto-generated method stub
-		return false;
+		if (getClusterName() != null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#isLocal()
+	 */
 	@Override
 	public boolean isLocal() {
-		// TODO Auto-generated method stub
-		return false;
+		return server_.isLocal();
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#isSocial()
+	 */
 	@Override
 	public boolean isSocial() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesign(org.openntf.domino.Database)
+	 */
 	@Override
 	public void refreshDesign(Database db) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesign(db.getFilePath());
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesign(org.openntf.domino.Database, boolean)
+	 */
 	@Override
 	public void refreshDesign(Database db, boolean refresh) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesign(db.getFilePath(), refresh);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesign(java.lang.String)
+	 */
 	@Override
 	public void refreshDesign(String dbFilePath) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesign(dbFilePath);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesign(java.lang.String, boolean)
+	 */
 	@Override
 	public void refreshDesign(String dbFilePath, boolean refresh) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesign(dbFilePath, refresh);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromLocal(org.openntf.domino.Database)
+	 */
 	@Override
 	public void refreshDesignFromLocal(Database db) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromLocal(db.getFilePath());
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromLocal(org.openntf.domino.Database, boolean)
+	 */
 	@Override
 	public void refreshDesignFromLocal(Database db, boolean refresh) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromLocal(db.getFilePath(), refresh);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromLocal(java.lang.String)
+	 */
 	@Override
 	public void refreshDesignFromLocal(String dbFilePath) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromLocal(dbFilePath);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromLocal(java.lang.String, boolean)
+	 */
 	@Override
 	public void refreshDesignFromLocal(String dbFilePath, boolean refresh) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromLocal(dbFilePath, refresh);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromServer(org.openntf.domino.Database, java.lang.String)
+	 */
 	@Override
 	public void refreshDesignFromServer(Database db, String serverName) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromServer(db.getFilePath(), serverName);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromServer(org.openntf.domino.Database, java.lang.String, boolean)
+	 */
 	@Override
 	public void refreshDesignFromServer(Database db, String serverName, boolean refresh) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromServer(db.getFilePath(), serverName, refresh);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromServer(java.lang.String, java.lang.String)
+	 */
 	@Override
 	public void refreshDesignFromServer(String dbFilePath, String serverName) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromServer(dbFilePath, serverName);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#refreshDesignFromServer(java.lang.String, java.lang.String, boolean)
+	 */
 	@Override
 	public void refreshDesignFromServer(String dbFilePath, String serverName, boolean refresh) {
-		// TODO Auto-generated method stub
-
+		try {
+			server_.refreshDesignFromServer(dbFilePath, serverName, refresh);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.openntf.domino.Server#sendConsoleCommand(java.lang.String)
+	 */
 	@Override
 	public String sendConsoleCommand(String command) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return server_.sendConsoleCommand(command);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
 	}
 
 }
