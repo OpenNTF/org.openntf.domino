53d23d7d567560cc48ddbc94800dc334f48c5705
Jesse Gallagher <jesse@secondfoundation.org>
2013-04-01 18:09:08 -0400

    Improved XSPUtil and added some more types for Factory.getParentDatabase

--- org.openntf.domino/src/org/openntf/domino/utils/XSPUtil.java
+++ org.openntf.domino/src/org/openntf/domino/utils/XSPUtil.java
@@ -5,6 +5,10 @@ import java.lang.reflect.Method;
 import org.openntf.domino.Database;
 import org.openntf.domino.Document;
 import org.openntf.domino.Session;
+import org.openntf.domino.View;
+import org.openntf.domino.ViewEntry;
+import org.openntf.domino.ViewEntryCollection;
+import org.openntf.domino.ViewNavigator;
 
 public enum XSPUtil {
 	INSTANCE;
@@ -20,26 +24,71 @@ public enum XSPUtil {
 		}
 	}
 
-	public static Database getCurrentDatabase() throws Exception {
+	public static ViewEntry wrap(lotus.domino.ViewEntry entry) {
 		try {
-			lotus.domino.Database db = (lotus.domino.Database) resolveVariable("database");
+			Object parent = entry.getParent();
+			lotus.domino.View view;
+			if (parent instanceof lotus.domino.ViewEntryCollection) {
+				view = ((lotus.domino.ViewEntryCollection) parent).getParent();
+			} else if (parent instanceof lotus.domino.ViewNavigator) {
+				view = ((lotus.domino.ViewNavigator) parent).getParentView();
+			} else {
+				view = (lotus.domino.View) parent;
+			}
+			lotus.domino.Database db = view.getParent();
 			Session session = Factory.fromLotus(db.getParent(), Session.class, null);
-			return Factory.fromLotus(db, Database.class, session);
+			Database wrappedDB = Factory.fromLotus(db, Database.class, session);
+			View wrappedView = Factory.fromLotus(view, View.class, wrappedDB);
+			if (parent instanceof lotus.domino.ViewEntryCollection) {
+				ViewEntryCollection vec = Factory.fromLotus((lotus.domino.Base) parent, ViewEntryCollection.class, wrappedView);
+				return Factory.fromLotus(entry, ViewEntry.class, vec);
+			} else if (parent instanceof lotus.domino.ViewNavigator) {
+				ViewNavigator vnav = Factory.fromLotus((lotus.domino.Base) parent, ViewNavigator.class, wrappedView);
+				return Factory.fromLotus(entry, ViewEntry.class, vnav);
+			} else {
+				return Factory.fromLotus(entry, ViewEntry.class, wrappedView);
+			}
 		} catch (lotus.domino.NotesException ne) {
 			return null;
 		}
 	}
 
-	public static Session getCurrentSession() throws Exception {
-		return Factory.fromLotus((lotus.domino.Session) resolveVariable("session"), Session.class, null);
+	public static Database getCurrentDatabase() {
+		try {
+			lotus.domino.Database db = (lotus.domino.Database) resolveVariable("database");
+			Session session = Factory.fromLotus(db.getParent(), Session.class, null);
+			return Factory.fromLotus(db, Database.class, session);
+		} catch (Exception ne) {
+			DominoUtils.handleException(ne);
+			return null;
+		}
 	}
 
-	public static Session getCurrentSessionAsSigner() throws Exception {
-		return Factory.fromLotus((lotus.domino.Session) resolveVariable("sessionAsSigner"), Session.class, null);
+	public static Session getCurrentSession() {
+		try {
+			return Factory.fromLotus((lotus.domino.Session) resolveVariable("session"), Session.class, null);
+		} catch (Exception ne) {
+			DominoUtils.handleException(ne);
+			return null;
+		}
 	}
 
-	public static Session getCurrentSessionAsSignerWithFullAccess() throws Exception {
-		return Factory.fromLotus((lotus.domino.Session) resolveVariable("sessionAsSignerWithFullAccess"), Session.class, null);
+	public static Session getCurrentSessionAsSigner() {
+		try {
+			return Factory.fromLotus((lotus.domino.Session) resolveVariable("sessionAsSigner"), Session.class, null);
+		} catch (Exception ne) {
+			DominoUtils.handleException(ne);
+			return null;
+		}
+	}
+
+	public static Session getCurrentSessionAsSignerWithFullAccess() {
+		try {
+			return Factory.fromLotus((lotus.domino.Session) resolveVariable("sessionAsSignerWithFullAccess"), Session.class, null);
+		} catch (Exception ne) {
+			DominoUtils.handleException(ne);
+			return null;
+		}
 	}
 
 	public static Object resolveVariable(String varName) throws Exception {
