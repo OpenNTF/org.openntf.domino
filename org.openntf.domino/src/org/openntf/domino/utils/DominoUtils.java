a5a60017ffb1a33376eb34b8ef24cb25ffd44aa3
Paul Withers <pwithers@intec.co.uk>
2013-04-04 20:12:12 +0100

    Fix for backup logger, fix for OpenLogItem

--- org.openntf.domino/src/org/openntf/domino/utils/DominoUtils.java
+++ org.openntf.domino/src/org/openntf/domino/utils/DominoUtils.java
@@ -59,7 +59,7 @@ import org.openntf.domino.logging.LogUtils;
 public enum DominoUtils {
 	;
 	private final static Logger log_ = Logger.getLogger("org.openntf.domino");
-	private final static Logger logBackup_ = Logger.getLogger("org.openntf.domino");
+	private final static Logger logBackup_ = Logger.getLogger("com.ibm.xsp.domino");
 
 	/**
 	 * Checksum.
@@ -402,7 +402,9 @@ public enum DominoUtils {
 		Object result = null;
 		lotus.domino.Stream mimeStream = session.createStream();
 		lotus.domino.MIMEEntity entity = doc.getMIMEEntity(itemName);
-		if(entity == null) { return null; }
+		if (entity == null) {
+			return null;
+		}
 		entity.getContentAsBytes(mimeStream);
 
 		ByteArrayOutputStream streamOut = new ByteArrayOutputStream();
