ebb57d7d69addbb796e3b6fd1f877eb5302f3dd1
Nathan T Freeman <nathan.freeman@redpilldevelopment.com>
2013-04-28 20:05:18 -0400

    Schema framing continued, and creation of org.openntf.domino.ext.Formula interface
    Signed-off-by: Nathan T Freeman <nathan.freeman@redpilldevelopment.com>

--- org.openntf.domino/src/org/openntf/domino/schema/DatabaseSchema.java
+++ org.openntf.domino/src/org/openntf/domino/schema/DatabaseSchema.java
@@ -3,7 +3,10 @@
  */
 package org.openntf.domino.schema;
 
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 import org.openntf.domino.Document;
 
@@ -15,9 +18,38 @@ public class DatabaseSchema {
 	private static final Logger log_ = Logger.getLogger(DatabaseSchema.class.getName());
 	private static final long serialVersionUID = 1L;
 
+	public static enum Flags {
+		SUMMARY, READERS, AUTHORS, PROTECTED, SIGNED, ENCRYPTED
+	}
+
+	static class DocumentDefinition {
+		private String name_; // will be used as Form field value
+		private Set<ItemDefinition> itemDefinitions_;
+		private Map<String, String> overrideLabels_;
+	}
+
 	static class ItemDefinition {
 		private String name_;
-		private String uiLabel_;
+		private String defaultLabel_;
+		private Class<?> type_;
+		private Set<Flags> flags_;
+		private Object defaultValue_;
+		private ItemValidation validator_;
+	}
+
+	static class ItemValidation {
+		private boolean required_;
+		private boolean unique_;
+		private String uniqueFormula_;
+		private Pattern expression_;
+		private long maxValue_;
+		private long minValue_;
+		private int maxMembers_;
+		private int minMembers_;
+
+		public void setRegex(String expression) {
+			expression_ = Pattern.compile(expression);
+		}
 
 	}
 
