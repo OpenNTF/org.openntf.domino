1c8515b268cccc60acefa596af915d9b85449662
Nathan T Freeman <nathan.freeman@redpilldevelopment.com>
2013-05-30 14:12:58 -0400

    Fixed copyright
    Signed-off-by: Nathan T Freeman <nathan.freeman@redpilldevelopment.com>

--- org.openntf.domino/src/org/openntf/domino/impl/DxlImporter.java
+++ org.openntf.domino/src/org/openntf/domino/impl/DxlImporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright OpenNTF 2013
+ * Copyright 2013
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
--- org.openntf.domino/src/org/openntf/domino/logging/OpenLogItem.java
+++ org.openntf.domino/src/org/openntf/domino/logging/OpenLogItem.java
@@ -2,7 +2,7 @@
 <<<<<<< HEAD
  * Copyright Paul Withers, Intec 2011-2013
 =======
- * Copyright OpenNTF 2013
+ * Copyright 2013
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
