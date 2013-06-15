7174660f527f3649e57fd1789a44e27691c2a9f4
Jesse Gallagher <jesse@frostillic.us>
2013-06-15 19:43:32 -0400

    Moved form field manipulation to a FormFieldList class instead of methods on DesignForm

--- org.openntf.domino/src/org/openntf/domino/design/DesignForm.java
+++ org.openntf.domino/src/org/openntf/domino/design/DesignForm.java
@@ -3,18 +3,10 @@
  */
 package org.openntf.domino.design;
 
-import java.util.List;
-
 /**
  * @author jgallagher
  * 
  */
 public interface DesignForm extends DesignBaseNamed {
-	public List<FormField> getFields();
-
-	public void removeField(int index);
-
-	public void swapFields(int a, int b);
-
-	public FormField addField();
+	public FormFieldList getFields();
 }
