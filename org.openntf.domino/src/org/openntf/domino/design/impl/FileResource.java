9266ba649313b7e56e10ec0da911a53977cc88c7
Jesse Gallagher <jesse@secondfoundation.org>
2013-06-24 19:46:51 -0400

    Switched FileResource to properly read and write CD records

--- org.openntf.domino/src/org/openntf/domino/design/impl/FileResource.java
+++ org.openntf.domino/src/org/openntf/domino/design/impl/FileResource.java
@@ -3,12 +3,16 @@ package org.openntf.domino.design.impl;
 import static javax.xml.bind.DatatypeConverter.parseBase64Binary;
 import static javax.xml.bind.DatatypeConverter.printBase64Binary;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.openntf.domino.Database;
 import org.openntf.domino.Document;
+import org.openntf.domino.design.cd.CDResourceFile;
 import org.openntf.domino.utils.DominoUtils;
 import org.openntf.domino.utils.xml.XMLNode;
 
@@ -36,25 +40,81 @@ public class FileResource extends AbstractDesignNoteBase implements org.openntf.
 
 	@Override
 	public byte[] getFileData() {
-		return parseBase64Binary(getFileDataNode().getTextContent());
+		try {
+			ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+			for (XMLNode rawitemdata : getDxl().selectNodes("//item[@name='$FileData']/rawitemdata")) {
+				String rawData = rawitemdata.getText();
+				byte[] thisData = parseBase64Binary(rawData);
+				byteStream.write(thisData);
+			}
+			byte[] data = byteStream.toByteArray();
+
+			CDResourceFile resourceFile = new CDResourceFile(data);
+			return resourceFile.getData();
+		} catch (IOException ioe) {
+			DominoUtils.handleException(ioe);
+			return null;
+		}
 	}
 
 	@Override
 	public void setFileData(final byte[] fileData) {
-		String base64 = printBase64Binary(fileData);
-		getFileDataNode().setTextContent("\n" + base64 + "\n");
+		try {
+			// To set the file content, first clear out existing content
+			List<XMLNode> fileDataNodes = getDxl().selectNodes("//item[@name='$FileData']");
+			for (int i = fileDataNodes.size() - 1; i >= 0; i++) {
+				fileDataNodes.get(i).getParentNode().removeChild(fileDataNodes.get(i));
+			}
 
-		// Also set the file size
-		XMLNode fileSizeNode = getDocumentElement().selectSingleNode("//item[@name='$FileSize']");
-		if (fileSizeNode == null) {
-			fileSizeNode = getDocumentElement().addChildElement("item");
-			fileSizeNode.setAttribute("name", "$FileSize");
-			fileSizeNode.setAttribute("sign", "true");
-			fileSizeNode = fileSizeNode.addChildElement("number");
-		} else {
-			fileSizeNode = fileSizeNode.selectSingleNode("number");
+			// Now create a CD record for the file data
+			CDResourceFile record = CDResourceFile.fromFileData(fileData, "");
+			byte[] reconData = record.getBytes();
+
+			// Write out the first chunk
+			int firstChunk = reconData.length > 20544 ? 20544 : reconData.length;
+			String firstChunkData = printBase64Binary(Arrays.copyOfRange(reconData, 0, firstChunk));
+			XMLNode documentNode = getDxl().selectSingleNode("//note");
+			XMLNode fileDataNode = documentNode.addChildElement("item");
+			fileDataNode.setAttribute("name", "$FileData");
+			fileDataNode = fileDataNode.addChildElement("rawitemdata");
+			fileDataNode.setAttribute("type", "1");
+			fileDataNode.setText(firstChunkData);
+
+			// Write out any remaining chunks
+			int remaining = reconData.length - firstChunk;
+			int chunks = remaining / 20516;
+			if (remaining % 20516 > 0) {
+				chunks++;
+			}
+			int offset = firstChunk;
+			for (int i = 0; i < chunks; i++) {
+				int chunkSize = remaining > 20516 ? 20516 : remaining;
+				String chunkData = printBase64Binary(Arrays.copyOfRange(reconData, offset, offset + chunkSize));
+
+				fileDataNode = documentNode.addChildElement("item");
+				fileDataNode.setAttribute("name", "$FileData");
+				fileDataNode = fileDataNode.addChildElement("rawitemdata");
+				fileDataNode.setAttribute("type", "1");
+				fileDataNode.setText(chunkData);
+
+				remaining -= 20516;
+				offset += chunkSize;
+			}
+
+			// Also set the file size
+			XMLNode fileSizeNode = getDocumentElement().selectSingleNode("//item[@name='$FileSize']");
+			if (fileSizeNode == null) {
+				fileSizeNode = getDocumentElement().addChildElement("item");
+				fileSizeNode.setAttribute("name", "$FileSize");
+				fileSizeNode.setAttribute("sign", "true");
+				fileSizeNode = fileSizeNode.addChildElement("number");
+			} else {
+				fileSizeNode = fileSizeNode.selectSingleNode("number");
+			}
+			fileSizeNode.setText(String.valueOf(fileData.length));
+		} catch (IOException ioe) {
+			DominoUtils.handleException(ioe);
 		}
-		fileSizeNode.setText(String.valueOf(fileData.length));
 	}
 
 	@Override
@@ -91,19 +151,6 @@ public class FileResource extends AbstractDesignNoteBase implements org.openntf.
 
 	public boolean isDeployable() {
 		return getFlagsExt().contains(DESIGN_FLAGEXT_FILE_DEPLOYABLE);
-	}
-
-	private XMLNode getFileDataNode() {
-		XMLNode dataNode = getDxl().selectSingleNode("//item[@name='$FileData']");
-		if (dataNode == null) {
-			dataNode = getDocumentElement().addChildElement("item");
-			dataNode.setAttribute("name", "$FileData");
-			dataNode = dataNode.addChildElement("rawitemdata");
-			dataNode.setAttribute("type", "1");
-		} else {
-			dataNode = dataNode.selectSingleNode("rawitemdata");
-		}
-		return dataNode;
 	}
 
 	private XMLNode getMimeTypeNode() {
