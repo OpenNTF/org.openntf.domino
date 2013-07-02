dce4a034ba93a1c0c5e67b5192970a20d1db04b9
Jesse Gallagher <jesse@frostillic.us>
2013-07-02 08:59:39 -0400

    Fixed a potential DateTime handle leak in Document#replaceItemValue when passing it a List of Dates + other objects

--- org.openntf.domino/src/org/openntf/domino/impl/Document.java
+++ org.openntf.domino/src/org/openntf/domino/impl/Document.java
@@ -739,8 +739,8 @@ public class Document extends Base<org.openntf.domino.Document, lotus.domino.Doc
 	@Override
 	public Vector<org.openntf.domino.EmbeddedObject> getEmbeddedObjects() {
 		try {
-			return Factory.fromLotusAsVector(getDelegate().getEmbeddedObjects(), org.openntf.domino.EmbeddedObject.class,
-					this.getAncestorSession());
+			return Factory.fromLotusAsVector(getDelegate().getEmbeddedObjects(), org.openntf.domino.EmbeddedObject.class, this
+					.getAncestorSession());
 		} catch (NotesException e) {
 			DominoUtils.handleException(e);
 		}
@@ -1911,6 +1911,8 @@ public class Document extends Base<org.openntf.domino.Document, lotus.domino.Doc
 						} else {
 							if (!objectClass.equals(domNode.getClass())) {
 								// Domino only allows uniform lists
+								// There may have been some native DateTimes added in so far; burn them
+								DominoUtils.incinerate(resultList);
 								throw new IllegalArgumentException();
 							}
 						}
@@ -2122,7 +2124,8 @@ public class Document extends Base<org.openntf.domino.Document, lotus.domino.Doc
 	 * @see org.openntf.domino.Document#replaceItemValueCustomDataBytes(java.lang.String, java.lang.String, byte[])
 	 */
 	@Override
-	public Item replaceItemValueCustomDataBytes(final String itemName, final String dataTypeName, final byte[] byteArray) throws IOException {
+	public Item replaceItemValueCustomDataBytes(final String itemName, final String dataTypeName, final byte[] byteArray)
+			throws IOException {
 		markDirty();
 		try {
 			if (byteArray.length > 65535) {
@@ -2401,13 +2404,13 @@ public class Document extends Base<org.openntf.domino.Document, lotus.domino.Doc
 			try {
 				lotus.domino.Document del = getDelegate().getParentDatabase().getDocumentByUNID(unid);
 				if (del != null) { // this is surprising. Why didn't we already get it?
-					log_.log(Level.WARNING,
-							"Document " + unid + " already existed in the database with noteid " + del.getNoteID()
-									+ " and we're trying to set a doc with noteid " + getNoteID() + " to that. The existing document is a "
-									+ del.getItemValueString("form") + " and the new document is a " + getItemValueString("form"));
+					log_.log(Level.WARNING, "Document " + unid + " already existed in the database with noteid " + del.getNoteID()
+							+ " and we're trying to set a doc with noteid " + getNoteID() + " to that. The existing document is a "
+							+ del.getItemValueString("form") + " and the new document is a " + getItemValueString("form"));
 					if (isDirty()) { // we've already made other changes that we should tuck away...
-						log_.log(Level.WARNING,
-								"Attempting to stash changes to this document to apply to other document of the same UNID. This is pretty dangerous...");
+						log_
+								.log(Level.WARNING,
+										"Attempting to stash changes to this document to apply to other document of the same UNID. This is pretty dangerous...");
 						Document stashDoc = copyToDatabase(getParentDatabase());
 						setDelegate(del);
 						for (Item item : stashDoc.getItems()) {
@@ -2593,18 +2596,16 @@ public class Document extends Base<org.openntf.domino.Document, lotus.domino.Doc
 					if (log_.isLoggable(Level.FINER)) {
 						Throwable t = new Throwable();
 						StackTraceElement[] elements = t.getStackTrace();
-						log_.log(Level.FINER,
-								elements[0].getClassName() + "." + elements[0].getMethodName() + " ( line " + elements[0].getLineNumber()
-										+ ")");
-						log_.log(Level.FINER,
-								elements[1].getClassName() + "." + elements[1].getMethodName() + " ( line " + elements[1].getLineNumber()
-										+ ")");
-						log_.log(Level.FINER,
-								elements[2].getClassName() + "." + elements[2].getMethodName() + " ( line " + elements[2].getLineNumber()
-										+ ")");
+						log_.log(Level.FINER, elements[0].getClassName() + "." + elements[0].getMethodName() + " ( line "
+								+ elements[0].getLineNumber() + ")");
+						log_.log(Level.FINER, elements[1].getClassName() + "." + elements[1].getMethodName() + " ( line "
+								+ elements[1].getLineNumber() + ")");
+						log_.log(Level.FINER, elements[2].getClassName() + "." + elements[2].getMethodName() + " ( line "
+								+ elements[2].getLineNumber() + ")");
 					}
-					log_.log(Level.FINE,
-							"If you recently rollbacked a transaction and this document was included in the rollback, this outcome is normal.");
+					log_
+							.log(Level.FINE,
+									"If you recently rollbacked a transaction and this document was included in the rollback, this outcome is normal.");
 				}
 			} catch (NotesException e) {
 				DominoUtils.handleException(e);
