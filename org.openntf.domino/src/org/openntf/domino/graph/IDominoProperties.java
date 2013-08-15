4885a5007acf3ca63989d572df513b90e6932bf1
Nathan T Freeman <nathan.freeman@redpilldevelopment.com>
2013-08-14 22:16:20 -0400

    Forgot the IDominoProperties interface!
    Signed-off-by: Nathan T Freeman <nathan.freeman@redpilldevelopment.com>

---
+++ org.openntf.domino/src/org/openntf/domino/graph/IDominoProperties.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.openntf.domino.graph;
+
+/**
+ * @author nfreeman
+ * 
+ */
+public interface IDominoProperties {
+	public String getName();
+
+	public Class<?> getType();
+}
