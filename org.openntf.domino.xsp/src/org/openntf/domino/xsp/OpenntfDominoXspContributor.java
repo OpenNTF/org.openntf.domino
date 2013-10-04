96b240a6a0f702d180e333b04c50bfbf49052ccf
Roland Praml <praml@foconis.de>
2013-10-04 10:33:55 +0200

    FIX: FacesListener-Init moved from a getter in domino-faces-config.xml

--- org.openntf.domino.xsp/src/org/openntf/domino/xsp/OpenntfDominoXspContributor.java
+++ org.openntf.domino.xsp/src/org/openntf/domino/xsp/OpenntfDominoXspContributor.java
@@ -1,12 +1,5 @@
 package org.openntf.domino.xsp;
 
-import java.security.AccessController;
-import java.security.PrivilegedExceptionAction;
-
-import javax.faces.FactoryFinder;
-import javax.faces.lifecycle.Lifecycle;
-import javax.faces.lifecycle.LifecycleFactory;
-
 import com.ibm.xsp.library.XspContributor;
 
 public class OpenntfDominoXspContributor extends XspContributor {
@@ -15,22 +8,23 @@ public class OpenntfDominoXspContributor extends XspContributor {
 	public Object[][] getFactories() {
 		Object[][] result = new Object[][] { { "org.openntf.domino.xsp.helpers.DOMINO_IMPLICIT_OBJECT_FACTORY",
 				org.openntf.domino.xsp.helpers.OpenntfDominoImplicitObjectFactory2.class } };
-		try {
-			// Adding a phase listener is a restricted operation
-			AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
-				@Override
-				public Object run() throws Exception {
-					System.out.println("Loading OpenLog PhaseListener");
-					LifecycleFactory factory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
-					Lifecycle lifecycle = factory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
-					lifecycle.addPhaseListener(new XspOpenLogPhaseListener());
-					System.out.println("Loaded OpenLog PhaseListener");
-					return null;
-				}
-			});
-		} catch (Throwable t) {
-			t.printStackTrace();
-		}
+
+		// try {
+		// // Adding a phase listener is a restricted operation
+		// AccessController.doPrivileged(new PrivilegedExceptionAction<Object>() {
+		// @Override
+		// public Object run() throws Exception {
+		// System.out.println("Loading OpenLog PhaseListener");
+		// LifecycleFactory factory = (LifecycleFactory) FactoryFinder.getFactory(FactoryFinder.LIFECYCLE_FACTORY);
+		// Lifecycle lifecycle = factory.getLifecycle(LifecycleFactory.DEFAULT_LIFECYCLE);
+		// lifecycle.addPhaseListener(new XspOpenLogPhaseListener());
+		// System.out.println("Loaded OpenLog PhaseListener");
+		// return null;
+		// }
+		// });
+		// } catch (Throwable t) {
+		// t.printStackTrace();
+		// }
 		return result;
 	}
 }
