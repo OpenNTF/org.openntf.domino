a2b4c38d26ad200260f199baa745759ad90f05b7
Roland Praml <praml@foconis.de>
2013-10-14 10:32:08 +0200

    FIX: Project setup

--- org.openntf.domino.plugin/src/org/openntf/domino/plugin/Activator.java
+++ org.openntf.domino.plugin/src/org/openntf/domino/plugin/Activator.java
@@ -1,27 +1,24 @@
 package org.openntf.domino.plugin;
 
-import org.eclipse.core.runtime.Plugin;
 import org.openntf.domino.utils.Factory;
+import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-public class Activator extends Plugin { // TODO: Warum nicht bundleActivator
+public class Activator implements BundleActivator {
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
-	@Override
 	public void start(final BundleContext context) throws Exception {
 		System.out.println("Loading OpenNTF Domino API");
 		Factory.setClassLoader(Thread.currentThread().getContextClassLoader());
-		System.out.println("... done");
 	}
 
 	/*
 	 * (non-Javadoc)
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
-	@Override
 	public void stop(final BundleContext context) throws Exception {
 		System.out.println("Stopping OpenNTF Domino API");
 	}
