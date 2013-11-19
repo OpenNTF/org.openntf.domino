9661cfe9f4c645f16da465fbaf2a5be51145695f
Nathan T. Freeman <nathan.freeman@redpilldevelopment.com>
2013-11-19 17:31:33 -0500

    Addition of org.openntf.domino.xsp.helpers.NSA, which allows an application to publish all its Modules and Sessions to a central servlet. Tweak to logging on Document.save() Untested addition of ViewNavigatorSiblingIterator. Still need harness to bind this together. Modification of TypeUtils so an item with a value of "" will return a null rather than an exception when a java.util.Date is requested.

--- org.openntf.domino/src/org/openntf/domino/iterators/ViewNavigatorSiblingIterator.java
+++ org.openntf.domino/src/org/openntf/domino/iterators/ViewNavigatorSiblingIterator.java
@@ -12,8 +12,8 @@ import org.openntf.domino.ViewNavigator;
  * @author Nathan T. Freeman
  * 
  */
-public class ViewNavigatorEntryIterator extends AbstractDominoIterator<ViewEntry> {
-	private static final Logger log_ = Logger.getLogger(ViewNavigatorEntryIterator.class.getName());
+public class ViewNavigatorSiblingIterator extends AbstractDominoIterator<ViewEntry> {
+	private static final Logger log_ = Logger.getLogger(ViewNavigatorSiblingIterator.class.getName());
 	private static final long serialVersionUID = 1L;
 
 	private transient ViewNavigator navigator_;
@@ -41,15 +41,25 @@ public class ViewNavigatorEntryIterator extends AbstractDominoIterator<ViewEntry
 	 * @param navigator
 	 *            the navigator
 	 */
-	public ViewNavigatorEntryIterator(final ViewNavigator navigator) {
+	public ViewNavigatorSiblingIterator(final ViewNavigator navigator) {
 		super(navigator);
 		navigator_ = navigator;
 		//		// TODO replace this with a less-expensive operation
+		//		count_ = navigator.getCount();
 	}
 
 	public ViewNavigator getNavigator() {
 		return navigator_;
 	}
+
+	//	/**
+	//	 * Gets the current entry.
+	//	 * 
+	//	 * @return the current entry
+	//	 */
+	//	public ViewEntry getCurrentEntry() {
+	//		return currentEntry_;
+	//	}
 
 	/*
 	 * (non-Javadoc)
@@ -58,12 +68,30 @@ public class ViewNavigatorEntryIterator extends AbstractDominoIterator<ViewEntry
 	 */
 	public boolean hasNext() {
 		if (currentEntry_ != null) {
-			nextEntry_ = getNavigator().getNextSibling(currentEntry_);
+			nextEntry_ = getNavigator().getNext(currentEntry_);
 		} else {
 			nextEntry_ = getNavigator().getFirst();
 		}
 		return nextEntry_ != null;
 	}
+
+	//	/**
+	//	 * Checks if is done.
+	//	 * 
+	//	 * @return true, if is done
+	//	 */
+	//	public boolean isDone() {
+	//		return done_;
+	//	}
+	//
+	//	/**
+	//	 * Checks if is started.
+	//	 * 
+	//	 * @return true, if is started
+	//	 */
+	//	public boolean isStarted() {
+	//		return started_;
+	//	}
 
 	/*
 	 * (non-Javadoc)
@@ -71,6 +99,7 @@ public class ViewNavigatorEntryIterator extends AbstractDominoIterator<ViewEntry
 	 * @see java.util.Iterator#next()
 	 */
 	public ViewEntry next() {
+		//		started_ = true;
 		ViewEntry result = null;
 		if (hasNext()) {
 			previousEntry_ = currentEntry_;	//TODO NTF use if we want to implement a ListIterator
@@ -91,4 +120,35 @@ public class ViewNavigatorEntryIterator extends AbstractDominoIterator<ViewEntry
 		// NOOP
 	}
 
+	//	/**
+	//	 * Sets the current entry.
+	//	 * 
+	//	 * @param currentEntry
+	//	 *            the new current entry
+	//	 */
+	//	public void setCurrentEntry(final ViewEntry currentEntry) {
+	//		currentEntry_ = currentEntry;
+	//		setStarted(currentEntry != null);
+	//		setDone(currentEntry == null);
+	//	}
+	//
+	//	/**
+	//	 * Sets the done.
+	//	 * 
+	//	 * @param done
+	//	 *            the new done
+	//	 */
+	//	public void setDone(final boolean done) {
+	//		done_ = done;
+	//	}
+	//
+	//	/**
+	//	 * Sets the started.
+	//	 * 
+	//	 * @param started
+	//	 *            the new started
+	//	 */
+	//	public void setStarted(final boolean started) {
+	//		started_ = started;
+	//	}
 }
