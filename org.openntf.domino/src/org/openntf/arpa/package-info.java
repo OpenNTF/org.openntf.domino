b968a9bff5090f065a2cb32a9f1105bcdbaffa2b
Devin S. Olson <dolson@czarnowski.com>
2014-01-10 16:05:27 -0500

    Added copyright and Apache License blocks

--- org.openntf.domino/src/org/openntf/arpa/package-info.java
+++ org.openntf.domino/src/org/openntf/arpa/package-info.java
@@ -1,9 +1,9 @@
 /**
  * ARPA utilities
  * 
- * @author Devin S. Olson (dolson@czarnowski.com)
- * 
  * Copyright 2014
+ * 
+ * @author Devin S. Olson (dolson@czarnowski.com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); 
  * you may not use this file except in compliance with the License. 
