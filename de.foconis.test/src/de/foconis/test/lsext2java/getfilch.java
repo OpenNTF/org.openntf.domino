c4b6a617b20c65ac326f4180ebb9e2b3dde3336a
Manfred Steinsiek <manfred.steinsiek@foconis.de>
2014-05-14 14:21:23 +0200

    More test cases for formula engine

--- de.foconis.test/src/de/foconis/test/lsext2java/getfilch.java
+++ de.foconis.test/src/de/foconis/test/lsext2java/getfilch.java
@@ -46,18 +46,11 @@ public class getfilch {
 		else if (!f.isFile())
 			errMsg = "File '" + fileName + "' isn't a normal file";
 		else {
-			fileLh = f.length();
-			try {
-				FileInputStream fis = new FileInputStream(f);
-				CRC32 crc = new CRC32();
-				byte[] buff = new byte[65536];
-				int got;
-				while ((got = fis.read(buff)) > 0)
-					crc.update(buff, 0, got);
-				fis.close();
-				fileCRC = crc.getValue();
-			} catch (Exception e) {
-				errMsg = "Exception " + e.getClass().getName() + ": " + e.getMessage();
+			long[] fileNums = new long[2];
+			fileNums[0] = f.length();
+			if ((errMsg = fileName.endsWith(".java") ? oneJavaFile(f, fileNums) : oneOtherFile(f, fileNums)) == null) {
+				fileLh = fileNums[0];
+				fileCRC = fileNums[1];
 			}
 		}
 		if (errMsg != null) {
@@ -70,6 +63,106 @@ public class getfilch {
 		fileEnt[2] = Long.toHexString(fileCRC);
 		iResList.add(fileEnt);
 		return true;
+	}
+
+	/*----------------------------------------------------------------------------*/
+	private String oneOtherFile(final File f, final long[] fileNums) {
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			CRC32 crc = new CRC32();
+			byte[] buff = new byte[65536];
+			int got;
+			while ((got = fis.read(buff)) > 0)
+				crc.update(buff, 0, got);
+			fis.close();
+			fileNums[1] = crc.getValue();
+			return null;
+		} catch (Exception e) {
+			return "getfilch: Exception " + e.getClass().getName() + ": " + e.getMessage();
+		}
+	}
+
+	/*----------------------------------------------------------------------------*/
+	private String oneJavaFile(final File f, final long[] fileNums) {
+		byte[] buff = new byte[(int) fileNums[0]];
+		try {
+			FileInputStream fis = new FileInputStream(f);
+			fis.read(buff);
+			fis.close();
+		} catch (Exception e) {
+			return "getfilch: Exception " + e.getClass().getName() + ": " + e.getMessage();
+		}
+		buff = normalizeJava(buff, fileNums);
+		CRC32 crc = new CRC32();
+		crc.update(buff, 0, (int) fileNums[0]);
+		fileNums[1] = crc.getValue();
+		return null;
+	}
+
+	/*----------------------------------------------------------------------------*/
+	private byte[] normalizeJava(final byte[] buff, final long[] fileNums) {
+		int lh = buff.length;
+		byte[] ret = new byte[lh];
+		int count = 0;
+		for (int i = 0; i < lh; i++) {
+			byte b = buff[i];
+			if (b == '/' && i < lh - 1 && buff[i + 1] == '/') {
+				for (; i < lh; i++) {
+					ret[count++] = buff[i];
+					if (buff[i] == '\n')
+						break;
+				}
+				continue;
+			}
+			if (b == '/' && i < lh - 1 && buff[i + 1] == '*') {
+				for (; i < lh - 1; i++) {
+					ret[count++] = buff[i];
+					if (buff[i] == '*' && buff[i + 1] == '/') {
+						ret[count++] = buff[++i];
+						break;
+					}
+				}
+				continue;
+			}
+			if (b == '"') {
+				boolean lastWasBS = false;
+				ret[count++] = buff[i++];
+				for (; i < lh; i++) {
+					ret[count++] = buff[i];
+					if (buff[i] == '"' && !lastWasBS)
+						break;
+					lastWasBS = (buff[i] == '\\');
+				}
+				continue;
+			}
+			if (b == '\'') {
+				boolean lastWasBS = false;
+				ret[count++] = buff[i++];
+				for (; i < lh; i++) {
+					ret[count++] = buff[i];
+					if (buff[i] == '\'' && !lastWasBS)
+						break;
+					lastWasBS = (buff[i] == '\\');
+				}
+				continue;
+			}
+			if (!Character.isWhitespace(b)) {
+				ret[count++] = b;
+				continue;
+			}
+			for (i++; i < lh; i++)
+				if (!Character.isWhitespace(buff[i]))
+					break;
+			if (i >= lh)
+				break;
+			if (Character.isJavaIdentifierStart(buff[i]) && count > 0
+					&& (Character.isLetterOrDigit(ret[count - 1]) || ret[count - 1] == '_'))
+				ret[count++] = ' ';
+			i--;
+		}
+		fileNums[0] = count;
+		// System.out.println(new String(ret, 0, count));
+		return ret;
 	}
 
 	/*----------------------------------------------------------------------------*/
