fa529e050a01c50a5cd5c40d91b3323952a8e2de
Devin S. Olson <dolson@czarnowski.com>
2014-06-05 17:01:57 -0400

    added override markers

--- org.openntf.domino/src/org/openntf/calendars/CalendarSet.java
+++ org.openntf.domino/src/org/openntf/calendars/CalendarSet.java
@@ -60,6 +60,7 @@ public class CalendarSet extends TreeSet<Calendar> implements CalendarRangeInter
 		return (null == calendar) ? false : super.add(calendar);
 	}
 
+	@Override
 	public boolean addObject(final Object object) {
 		if (null != object) {
 			final Calendar c = Dates.getCalendar(object);
@@ -77,6 +78,7 @@ public class CalendarSet extends TreeSet<Calendar> implements CalendarRangeInter
 	 * 
 	 * @return Flag indicating if this set changed as a result of the call.
 	 */
+	@Override
 	public boolean addAll(final Set<Calendar> set) {
 		if ((null == set) || set.isEmpty()) {
 			return false;
@@ -102,46 +104,57 @@ public class CalendarSet extends TreeSet<Calendar> implements CalendarRangeInter
 	 * ***************************************************
 	 * ***************************************************
 	 */
+	@Override
 	public boolean contains(final Calendar calendar) {
 		return super.contains(calendar);
 	}
 
+	@Override
 	public String getHoursMinutes() {
 		return this.toCalendarRange().getHoursMinutes();
 	}
 
+	@Override
 	public CalendarRange getIntersection(final CalendarRange cr) {
 		return this.toCalendarRange().getIntersection(cr);
 	}
 
+	@Override
 	public long getMinutes() {
 		return this.toCalendarRange().getMinutes();
 	}
 
+	@Override
 	public CalendarRange getUnion(final CalendarRange cr) {
 		return this.toCalendarRange().getUnion(cr);
 	}
 
+	@Override
 	public boolean isInRange(final Object object) {
 		return this.toCalendarRange().isInRange(object);
 	}
 
+	@Override
 	public boolean isInRange(final Object object, final boolean inclusive) {
 		return this.toCalendarRange().isInRange(object, inclusive);
 	}
 
+	@Override
 	public boolean isIntersectionExists(final CalendarRange cr) {
 		return this.toCalendarRange().isIntersectionExists(cr);
 	}
 
+	@Override
 	public boolean isUnionExists(final CalendarRange cr) {
 		return this.toCalendarRange().isUnionExists(cr);
 	}
 
+	@Override
 	public boolean isValid() {
 		return this.toCalendarRange().isValid();
 	}
 
+	@Override
 	public CalendarRange toCalendarRange() {
 		return new CalendarRange(this.first(), this.last());
 	}
