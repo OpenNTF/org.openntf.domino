5df228337b9f4dd1d7fe2d3cc3f3c5e2e9f5106f
Nathan T. Freeman <nathan.freeman@redpilldevelopment.com>
2014-06-27 19:41:51 -0400

    Introduction of Xots package and shell classes for XPages scheduled & triggered tasklet implementation Addition of View.IndexType enum to cover Shared, Private, Private on First Use, and other various index settings for views Added numerous boolean and getters to return View control values that are stored in $Flags and $Index items Bypasses delegate calls for .isCalendar() and .isFolder() to avoid inadvertent index builds. Added .isTimeSensitive() that detects presence of $FormulaTV item, which tells us that the view has time-based calculations in it.

---
+++ org.openntf.domino.xsp/src/org/openntf/domino/xots/XotsAbstractScheduledTasklet.java
@@ -0,0 +1,9 @@
+package org.openntf.domino.xots;
+
+public abstract class XotsAbstractScheduledTasklet extends XotsBaseTasklet implements XotsIScheduledTasklet {
+
+	public XotsAbstractScheduledTasklet() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
