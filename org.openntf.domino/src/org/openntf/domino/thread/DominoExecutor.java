664a8f25d2e9c7be9205ee5c3d7bcd214fe73c7b
Nathan T. Freeman <nathan.freeman@redpilldevelopment.com>
2014-07-14 14:10:00 -0400

    Continued development on Xots processing. Improved class hierarchy and extensibility

--- org.openntf.domino/src/org/openntf/domino/thread/DominoExecutor.java
+++ org.openntf.domino/src/org/openntf/domino/thread/DominoExecutor.java
@@ -3,14 +3,11 @@
  */
 package org.openntf.domino.thread;
 
-import java.security.AccessControlContext;
-import java.security.AccessController;
-import java.security.Permission;
-import java.security.PrivilegedAction;
 import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.RunnableFuture;
@@ -30,36 +27,22 @@ import org.openntf.domino.thread.model.IDominoRunnable;
 @Incomplete
 public class DominoExecutor extends ThreadPoolExecutor {
 
-	protected static class TrustedSecurityManager extends SecurityManager {
-		public TrustedSecurityManager() {
-			//			System.out.println("Made a new TrustedSecurityManager");
+	public static class OpenSecurityManager extends SecurityManager {
+		public OpenSecurityManager() {
 		}
-
-		@Override
-		public void checkPermission(final Permission paramPermission) {
-			//			System.out.println("Giving permission for " + paramPermission.getName());
-			//			super.checkPermission(paramPermission);
-		}
-
-		@Override
-		public void checkPermission(final Permission paramPermission, final Object paramObject) {
-			//			super.checkPermission(paramPermission, paramObject);
-		}
-
 	}
 
-	protected static class TrustedRunnable implements Runnable {
-		private final Runnable runnable_;
-		private final AccessControlContext acc_;
-		private final SecurityManager sm_;
+	public static class OpenRunnable implements Runnable {
+		protected final Runnable runnable_;
+		protected ClassLoader loader_;
 
-		private ClassLoader loader_;
-
-		public TrustedRunnable(final Runnable runnable, final AccessControlContext acc, final SecurityManager sm) {
-			//			System.out.println("Created new TrustedRunnable");
+		public OpenRunnable(final Runnable runnable) {
 			runnable_ = runnable;
-			acc_ = acc;
-			sm_ = sm;
+		}
+
+		public OpenRunnable(final Runnable runnable, final ClassLoader loader) {
+			runnable_ = runnable;
+			loader_ = loader;
 		}
 
 		public void setClassLoader(final ClassLoader loader) {
@@ -68,70 +51,39 @@ public class DominoExecutor extends ThreadPoolExecutor {
 
 		@Override
 		public void run() {
-			//			System.out.println("Running a " + runnable_.getClass().getName() + " with an AccessContext of " + acc_.getClass().getName()
-			//					+ " and a security manager of " + (sm_ == null ? "null" : sm_.getClass().getName()));
-			AccessController.doPrivileged(new PrivilegedAction<Object>() {
-				@Override
-				public Object run() {
-					SecurityManager oldSM = System.getSecurityManager();
-					try {
-						System.setSecurityManager(sm_);
-					} catch (Throwable t) {
-						t.printStackTrace();
-					}
-					if (null != loader_) {
-						Thread.currentThread().setContextClassLoader(loader_);
-					}
-					runnable_.run();
-					try {
-						//						System.out.println("Resetting securitymanager to " + (oldSM == null ? "null" : oldSM));
-						System.setSecurityManager(oldSM);
-					} catch (Throwable t) {
-						t.printStackTrace();
-					}
-					return null;
-
-				}
-			}, acc_);
+			if (null != loader_) {
+				Thread.currentThread().setContextClassLoader(loader_);
+			}
+			runnable_.run();
 		}
 	}
 
 	private static final Logger log_ = Logger.getLogger(DominoExecutor.class.getName());
 	private static final long serialVersionUID = 1L;
 	private Set<IDominoListener> listeners_;
-	private AccessControlContext factoryAccessController_;
-	private SecurityManager securityManager_ = new TrustedSecurityManager();
 
 	private static BlockingQueue<Runnable> getBlockingQueue(final int size) {
 		return new LinkedBlockingQueue<Runnable>(size);
 	}
 
-	private void init() {
-		//		System.out.println("New DominoExecutor constructed");
-		factoryAccessController_ = AccessController.getContext();
-		//		factoryAccessController_.checkPermission(new RuntimePermission("setContextClassLoader"));
-		//		securityManager_ = System.getSecurityManager();
-		//		System.out.println("SecurityManager is a " + (securityManager_ == null ? "null" : securityManager_.getClass().getName()));
+	protected void init() {
+
 	}
 
 	public DominoExecutor() {
 		this(50);
-		//		super(50, 50, 60l, TimeUnit.SECONDS, getBlockingQueue(50), new DominoThreadFactory());
 	}
 
 	public DominoExecutor(final int corePoolSize) {
 		this(corePoolSize, corePoolSize);
-		//		super(corePoolSize, corePoolSize, 60l, TimeUnit.SECONDS, getBlockingQueue(corePoolSize), new DominoThreadFactory());
 	}
 
 	public DominoExecutor(final int corePoolSize, final int maximumPoolSize) {
 		this(corePoolSize, maximumPoolSize, 60l);
-		//		super(corePoolSize, maximumPoolSize, 60l, TimeUnit.SECONDS, getBlockingQueue(maximumPoolSize), new DominoThreadFactory());
 	}
 
 	public DominoExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime) {
 		this(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, getBlockingQueue(maximumPoolSize));
-		//		super(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS, getBlockingQueue(maximumPoolSize), new DominoThreadFactory());
 	}
 
 	public DominoExecutor(final int paramInt1, final int paramInt2, final long paramLong, final TimeUnit paramTimeUnit,
@@ -197,28 +149,28 @@ public class DominoExecutor extends ThreadPoolExecutor {
 	 */
 	@Override
 	public void execute(Runnable runnable) {
-		if (runnable instanceof DominoNativeRunner) {
+		if (runnable instanceof OpenRunnable) {
+
+		} else if (runnable instanceof DominoNativeRunner) {
 			final ClassLoader loader = ((DominoNativeRunner) runnable).getClassLoader();
-			runnable = new TrustedRunnable((DominoNativeRunner) runnable, factoryAccessController_, securityManager_);
-			((TrustedRunnable) runnable).setClassLoader(loader);
+			runnable = new OpenRunnable(runnable);
+			((OpenRunnable) runnable).setClassLoader(loader);
 		} else if (runnable instanceof IDominoRunnable) {
 			DominoSessionType type = ((IDominoRunnable) runnable).getSessionType();
 			final ClassLoader loader = ((IDominoRunnable) runnable).getContextClassLoader();
 			if (type == DominoSessionType.NATIVE) {
 				DominoNativeRunner nativeRunner = new DominoNativeRunner(runnable, loader);
-				TrustedRunnable runner = new TrustedRunnable(nativeRunner, factoryAccessController_, securityManager_);
-				runner.setClassLoader(loader);
+				runnable = new OpenRunnable(nativeRunner);
+				((OpenRunnable) runnable).setClassLoader(loader);
 			} else {
 				System.out.println("IDominoRunnable has session type " + type.name());
 			}
 		}
-		RunnableFuture future = null;
-		if (runnable instanceof RunnableFuture) {
-			future = (RunnableFuture) runnable;
+		if (runnable instanceof Future) {
+			super.execute(runnable);
 		} else {
-			future = newTaskFor(runnable, null);
+			super.submit(runnable);
 		}
-		super.execute(future);
 	}
 
 	/* (non-Javadoc)
