a1f0f29c527d01ed5888d64aaf28e4e5dd8a01cb
Jesse Gallagher <jesse@frostillic.us>
2014-07-14 21:04:34 -0400

    Added a preliminary, not-exactly-functional JSR223 runner

---
+++ org.openntf.domino.xsp/src/org/openntf/domino/xots/JSR223Tasklet.java
@@ -0,0 +1,54 @@
+package org.openntf.domino.xots;
+
+import java.io.PrintWriter;
+
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import org.openntf.domino.Database;
+import org.openntf.domino.design.DatabaseDesign;
+import org.openntf.domino.design.FileResource;
+import org.openntf.domino.thread.DominoSessionType;
+
+public class JSR223Tasklet extends XotsBaseTasklet {
+	private static final long serialVersionUID = 1L;
+
+	private final String script_;
+	private final String scriptExt_;
+	private final String databasePath_;
+
+	public JSR223Tasklet(final String scriptName, final DominoSessionType type, final Database database) {
+		int extIndex = scriptName.lastIndexOf('.');
+		scriptExt_ = scriptName.substring(extIndex + 1);
+
+		DatabaseDesign design = database.getDesign();
+		FileResource script = design.getAnyFileResource(scriptName);
+		script_ = new String(script.getFileData());
+		databasePath_ = database.getApiPath();
+
+		setSessionType(type);
+	}
+
+	@Override
+	public void run() {
+		Database database = getSession().getDatabase(databasePath_);
+
+		ScriptEngineManager manager = new ScriptEngineManager(ScriptEngineManager.class.getClassLoader());
+		ScriptEngine engine = manager.getEngineByExtension(scriptExt_);
+
+		engine.put("database", database);
+		engine.put("session", database.getAncestorSession());
+
+		ScriptContext context = engine.getContext();
+		context.setWriter(new PrintWriter(System.out));
+		context.setErrorWriter(new PrintWriter(System.err));
+
+		try {
+			engine.eval(script_);
+		} catch (ScriptException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
