561359b98c23ba8d6ec4236faa05af045879de3f
Paul Withers <paulswithers@hotmail.co.uk>
2014-10-06 09:30:47 +0100

    Uncommenting NotesCalendar stuff for R9.0.1 FP2

--- org.openntf.domino/src/org/openntf/domino/impl/NotesCalendar.java
+++ org.openntf.domino/src/org/openntf/domino/impl/NotesCalendar.java
@@ -423,12 +423,12 @@ public class NotesCalendar extends Base<org.openntf.domino.NotesCalendar, lotus.
 	/* (non-Javadoc)
 	 * @see org.openntf.domino.NotesCalendar#getApptunidFromUID(java.lang.String, boolean)
 	 */
-	//	public String getApptunidFromUID(final String arg0, final boolean arg1) {
-	//		try {
-	//			return getDelegate().getApptunidFromUID(arg0, arg1);
-	//		} catch (NotesException e) {
-	//			DominoUtils.handleException(e);
-	//			return null;
-	//		}
-	//	}
+	public String getApptunidFromUID(final String arg0, final boolean arg1) {
+		try {
+			return getDelegate().getApptunidFromUID(arg0, arg1);
+		} catch (NotesException e) {
+			DominoUtils.handleException(e);
+			return null;
+		}
+	}
 }
