6563f4bf338126ef9779cc242df16f0014f1408f
Paul Withers <paulswithers@hotmail.co.uk>
2017-02-16 14:19:31 +0000

    Cherry-picking fix for autoboxing sql dates and times

--- domino/xsp/src/test/java/org/openntf/domino/xsp/tests/paul/Connect17Documents.java
+++ domino/xsp/src/test/java/org/openntf/domino/xsp/tests/paul/Connect17Documents.java
@@ -1,15 +1,23 @@
 package org.openntf.domino.xsp.tests.paul;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Vector;
 
 import org.openntf.domino.Database;
+import org.openntf.domino.DateTime;
 import org.openntf.domino.Document;
 import org.openntf.domino.DocumentCollection;
+import org.openntf.domino.Item;
 import org.openntf.domino.Session;
 import org.openntf.domino.View;
+import org.openntf.domino.ext.Session.Fixes;
 import org.openntf.domino.junit.TestRunnerUtil;
+import org.openntf.domino.types.AuthorsList;
+import org.openntf.domino.types.NamesList;
+import org.openntf.domino.types.ReadersList;
 import org.openntf.domino.utils.Factory;
 import org.openntf.domino.utils.Factory.SessionType;
 
@@ -30,7 +38,11 @@ public class Connect17Documents implements Runnable {
 		View contacts = extLib.getView("AllContacts");
 		View threads = extLib.getView("AllThreadsByAuthor");
 		Document doc = contacts.getFirstDocument();
-		resetDoc(doc);
+		resetDoc(doc);	// Clears changes this function already made
+
+		Document newDoc = extLib.createDocument();
+		doc.copyAllItems(newDoc, true);
+
 		String prevDocAsJson = doc.toJson(true);
 		doc.appendItemValue("State", "AZ");
 		doc.replaceItemValue("DateTimeField", new Date());
@@ -53,10 +65,38 @@ public class Connect17Documents implements Runnable {
 			mapField.put(tmp.getUniversalID(), tmp.getItemValueString("Title"));
 		}
 		doc.put("MapField", mapField);
+		BigDecimal decimal = new BigDecimal("2.5");
+		doc.replaceItemValue("BigDecimalField", decimal);
+		doc.replaceItemValue("EnumField", Fixes.FORCE_HEX_LOWER_CASE);
 		doc.save();
 		HashMap tmp = doc.getItemValue("MapField", HashMap.class);
 		System.out.println(tmp.size());
 		System.out.println(doc.getMetaversalID());
+		System.out.println(doc.getItemValueString("EnumField"));
+		java.sql.Date sqlDt = doc.getItemValue("DateTimeField", java.sql.Date.class);
+		System.out.println(sqlDt);
+		java.sql.Time sqlTime = doc.getItemValue("DateTimeField", java.sql.Time.class);
+		System.out.println(sqlTime);
+		System.out.println(doc.getItemValues("BigDecimalField", BigDecimal.class));
+
+		NamesList<String> names = new NamesList<String>();
+		names.add("CN=Paul Withers/O=Intec");
+		names.add("CN=Admin/O=Intec=PW");
+		newDoc.replaceItemValue("Names", names);
+		AuthorsList<String> authors = new AuthorsList<String>();
+		authors.addAll(names);
+		newDoc.replaceItemValue("Authors", authors);
+		ReadersList<String> readers = new ReadersList<String>();
+		readers.addAll(names);
+		newDoc.replaceItemValue("Readers", readers);
+		Item dt = newDoc.replaceItemValue("TestDate", "");
+		Vector<DateTime> dates = new Vector();
+		DateTime dt1 = sess.createDateTime("01/01/2017");
+		DateTime dt2 = sess.createDateTime("02/01/2017");
+		dates.add(dt1);
+		dates.add(dt2);
+		dt.setValues(dates);
+		newDoc.save();
 	}
 
 	public void resetDoc(final Document doc) {
@@ -65,6 +105,11 @@ public class Connect17Documents implements Runnable {
 		doc.removeItem("MVField");
 		doc.removeItem("DocAsJson");
 		doc.removeItem("MapField");
+		doc.removeItem("DateTimeField");
+		doc.removeItem("DateOnlyField");
+		doc.removeItem("TimeOnlyField");
+		doc.removeItem("BigDecimalField");
+		doc.removeItem("EnumField");
 		doc.save();
 	}
 
