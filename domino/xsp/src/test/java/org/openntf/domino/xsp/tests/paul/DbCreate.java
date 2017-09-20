ca9562536d31603aa03c839d85a820e41c5b1618
Paul Withers <pwithers@intec.co.uk>
2017-09-20 11:54:01 +0100

    Adding code and test for creating new blank database

---
+++ domino/xsp/src/test/java/org/openntf/domino/xsp/tests/paul/DbCreate.java
@@ -0,0 +1,33 @@
+package org.openntf.domino.xsp.tests.paul;
+
+import org.openntf.domino.Database;
+import org.openntf.domino.Session;
+import org.openntf.domino.junit.TestRunnerUtil;
+import org.openntf.domino.utils.Factory;
+import org.openntf.domino.utils.Factory.SessionType;
+
+public class DbCreate implements Runnable {
+
+	public DbCreate() {
+
+	}
+
+	public static void main(final String[] args) {
+		TestRunnerUtil.runAsDominoThread(new DbCreate(), TestRunnerUtil.NATIVE_SESSION);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void run() {
+		try {
+			Session sess = Factory.getSession(SessionType.NATIVE);
+			Database db = sess.createBlankDatabase("temp", "demo1.nsf");
+			System.out.println(db.getApiPath());
+			Database db2 = sess.createBlankDatabaseAbsolutePath("C:/PaulTemp/ODATest", "demo1.nsf");
+			System.out.println(db2.getApiPath());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
