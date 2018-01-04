32ef3440ab414d7d7cba3e5e8d369127c53751df
Paul Withers <pwithers@intec.co.uk>
2018-01-04 17:29:59 +0000

    Callback interfaces and helper methods for doing boilerplating for extracting HttpRequest and HttpResponse, processing, and properly terminating the response from an XAgent. One is specifically designed to allow population of a JSON object which will get passed back to the REST request
       /*
        * One method call to XspUtils.initialiseAndProcessResponse() does all the boilerplating for extracting the request, response and JsonJavaObject
        * and properly terminating the response. JsonJavaObject is basically the same as a Java Map. All gotchas are handled for you!
        *
        * The bit that could be new to most is that XspUtils.initialiseAndProcessResponseAsJson() takes an anonymous inner class as its method. This is just
        * a way to pass in a process() method that can interact with the request and response set up by XspUtils.initialiseAndProcessResponseAsJson()
        *
        * With Java 8 it becomes more readable with lambdas:
        *
        * XspUtils.initialiseAndProcessResponseAsJson((request, response, jsonObj) -> {
        *     // do stuff here
        * });
        */

--- domino/xsp/src/main/java/org/openntf/domino/xsp/helpers/XspUtils.java
+++ domino/xsp/src/main/java/org/openntf/domino/xsp/helpers/XspUtils.java
@@ -3,14 +3,27 @@
  */
 package org.openntf.domino.xsp.helpers;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.logging.Logger;
+
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.openntf.domino.Document;
 import org.openntf.domino.utils.DominoUtils;
 import org.openntf.domino.utils.XSPUtil;
+import org.openntf.domino.xsp.IXspHttpServletJsonResponseCallback;
+import org.openntf.domino.xsp.IXspHttpServletResponseCallback;
 import org.openntf.domino.xsp.ODAPlatform;
 
+import com.ibm.commons.util.io.json.JsonException;
+import com.ibm.commons.util.io.json.JsonJavaObject;
+import com.ibm.domino.services.HttpServiceConstants;
 import com.ibm.xsp.model.domino.wrapped.DominoDocument;
+import com.ibm.xsp.webapp.XspHttpServletResponse;
 
 /**
  * @author Nathan T. Freeman
@@ -53,6 +66,61 @@ public class XspUtils {
 			return null;
 		}
 		return beDoc;
+	}
+
+	/**
+	 * A generic method that performs boilerplate code to extract XspHttpServletRequest and HttpServletResponse; triggers a callback method
+	 * passed in giving it access to the request, response and a JsonJavaObject; then closes everything down successfully
+	 * 
+	 * @param callback
+	 *            anonymous inner class callback that implements IXspHttpServletResponse, so has a process() method that can be called from
+	 *            here
+	 * @throws IOException
+	 *             that may be caused by manipulating the response
+	 * @throws JsonException
+	 *             caused by malformed JSON, shouldn't happen
+	 * @since ODA 4.3.0
+	 */
+	public static void initialiseAndProcessResponseAsJson(final IXspHttpServletJsonResponseCallback callback)
+			throws IOException, JsonException {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		ExternalContext ext = ctx.getExternalContext();
+		XspHttpServletResponse response = (XspHttpServletResponse) ext.getResponse();
+		response.setContentType(HttpServiceConstants.CONTENTTYPE_APPLICATION_JSON);
+		response.setHeader("Cache-Control", "no-cache");
+		JsonJavaObject result = new JsonJavaObject();
+		callback.process((HttpServletRequest) ext.getRequest(), response, result);
+		if (!response.isStatusSet()) {
+			response.setStatus(HttpServletResponse.SC_OK);
+		}
+		PrintWriter writer = response.getWriter();
+		writer.write(result.toString());
+		//  Terminate the request processing lifecycle.
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+
+	/**
+	 * A more basic generic method that performs boilerplate code to extract XspHttpServletRequest and HttpServletResponse; triggers a
+	 * callback method passed in, passing it the request and response; then terminates the response
+	 * 
+	 * It's down to you to handle printing something to the response
+	 * 
+	 * @param callback
+	 *            anonymous inner class callback that implements IXspHttpServletResponse, so has a process() method that can be called from
+	 *            here
+	 * @throws IOException
+	 *             that may be caused by manipulating the response
+	 * @since ODA 4.3.0
+	 */
+	public static void initialiseAndProcessResponse(final IXspHttpServletResponseCallback callback) throws IOException {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		ExternalContext ext = ctx.getExternalContext();
+		XspHttpServletResponse response = (XspHttpServletResponse) ext.getResponse();
+		response.setContentType(HttpServiceConstants.CONTENTTYPE_APPLICATION_JSON);
+		response.setHeader("Cache-Control", "no-cache");
+		callback.process((HttpServletRequest) ext.getRequest(), response);
+		//  Terminate the request processing lifecycle.
+		FacesContext.getCurrentInstance().responseComplete();
 	}
 
 }
