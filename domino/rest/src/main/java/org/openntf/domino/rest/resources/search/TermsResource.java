6b979b7c9c6bebe47fc6819da3d5940d8519dd93
nathan t freeman <ntf@redpillnow.com>
2018-08-20 15:39:18 -0500

    no message

---
+++ domino/rest/src/main/java/org/openntf/domino/rest/resources/search/TermsResource.java
@@ -0,0 +1,217 @@
+package org.openntf.domino.rest.resources.search;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.EntityTag;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.openntf.domino.exceptions.UserAccessException;
+import org.openntf.domino.graph2.annotations.FramedEdgeList;
+import org.openntf.domino.graph2.annotations.FramedVertexList;
+import org.openntf.domino.graph2.annotations.MixedFramedVertexList;
+import org.openntf.domino.graph2.impl.DFramedTransactionalGraph;
+import org.openntf.domino.rest.json.JsonGraphWriter;
+import org.openntf.domino.rest.resources.AbstractResource;
+import org.openntf.domino.rest.service.ODAGraphService;
+import org.openntf.domino.rest.service.Parameters;
+import org.openntf.domino.rest.service.Parameters.ParamMap;
+import org.openntf.domino.rest.service.Routes;
+import org.openntf.domino.utils.DominoUtils;
+
+import com.ibm.commons.util.io.json.JsonException;
+import com.ibm.domino.das.utils.ErrorHelper;
+
+@Path(Routes.ROOT + "/" + Routes.TERMS + "/" + Routes.NAMESPACE_PATH_PARAM)
+public class TermsResource extends AbstractResource {
+
+	public TermsResource(final ODAGraphService service) {
+		super(service);
+	}
+
+	@SuppressWarnings("unchecked")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTermsObject(@Context final UriInfo uriInfo, @PathParam(Routes.NAMESPACE) final String namespace,
+			@Context final Request request) throws JsonException, IOException {
+		@SuppressWarnings("rawtypes")
+		DFramedTransactionalGraph graph = this.getGraph(namespace);
+		ParamMap pm = Parameters.toParamMap(uriInfo);
+		StringWriter sw = new StringWriter();
+		JsonGraphWriter writer = new JsonGraphWriter(sw, graph, pm, false, true, true);
+		try {
+			List<CharSequence> types = new ArrayList<CharSequence>();
+			types.add("org.openntf.domino.graph2.builtin.search.Term");
+			List<CharSequence> filterkeys = pm.getFilterKeys();
+			List<CharSequence> filtervalues = pm.getFilterValues();
+			List<CharSequence> partialkeys = pm.getPartialKeys();
+			List<CharSequence> partialvalues = pm.getPartialValues();
+			List<CharSequence> startskeys = pm.getStartsKeys();
+			List<CharSequence> startsvalues = pm.getStartsValues();
+
+			if (types.size() == 0) {
+				writer.outNull();
+			} else if (types.size() == 1) {
+				CharSequence typename = types.get(0);
+
+				Iterable<?> elements = null;
+				if (filterkeys != null) {
+					elements = graph.getFilteredElements(typename.toString(), filterkeys, filtervalues);
+				} else if (partialkeys != null) {
+					elements = graph.getFilteredElementsPartial(typename.toString(), partialkeys, partialvalues);
+				} else if (startskeys != null) {
+					elements = graph.getFilteredElementsStarts(typename.toString(), startskeys, startsvalues);
+				} else {
+					elements = graph.getElements(typename.toString());
+				}
+
+				if (elements instanceof FramedEdgeList) {
+					List<?> result = sortAndLimitList((List<?>) elements, pm);
+					writer.outArrayLiteral(result);
+				} else if (elements instanceof FramedVertexList) {
+					List<?> result = sortAndLimitList((List<?>) elements, pm);
+					writer.outArrayLiteral(result);
+				} else {
+					List<Object> maps = new ArrayList<Object>();
+					for (Object element : elements) {
+						maps.add(element);
+					}
+					writer.outArrayLiteral(maps);
+				}
+			} else {
+				MixedFramedVertexList vresult = null;
+				FramedEdgeList eresult = null;
+				for (CharSequence typename : types) {
+					Iterable<?> elements = null;
+					if (filterkeys != null) {
+						elements = graph.getFilteredElements(typename.toString(), filterkeys, filtervalues);
+					} else if (partialkeys != null) {
+						elements = graph.getFilteredElementsPartial(typename.toString(), partialkeys,
+								partialvalues);
+					} else if (startskeys != null) {
+						elements = graph.getFilteredElementsStarts(typename.toString(), startskeys, startsvalues);
+					} else {
+						elements = graph.getElements(typename.toString());
+					}
+					if (elements instanceof FramedVertexList) {
+						if (vresult == null) {
+							vresult = new MixedFramedVertexList(graph, null, (FramedVertexList) elements);
+						} else {
+							vresult.addAll((List<?>) elements);
+						}
+					} else if (elements instanceof FramedEdgeList) {
+						if (eresult == null) {
+							eresult = (FramedEdgeList) elements;
+						} else {
+							eresult.addAll((FramedEdgeList) elements);
+						}
+					}
+				}
+				if (vresult != null) {
+					List<?> result = sortAndLimitList(vresult, pm);
+					writer.outArrayLiteral(result);
+				}
+				if (eresult != null) {
+					List<?> result = sortAndLimitList(eresult, pm);
+					writer.outArrayLiteral(result);
+				}
+			}
+		} catch (UserAccessException uae) {
+			throw new WebApplicationException(ErrorHelper.createErrorResponse(uae, Response.Status.UNAUTHORIZED));
+		} catch (Exception e) {
+			throw new WebApplicationException(
+					ErrorHelper.createErrorResponse(e, Response.Status.INTERNAL_SERVER_ERROR));
+		}
+
+		String jsonEntity = sw.toString();
+		ResponseBuilder berg = getBuilder(jsonEntity, new Date(), true, request);
+		Response response = berg.build();
+		return response;
+	}
+
+	private ResponseBuilder getBuilder(final String jsonEntity, final Date lastMod, final boolean includeEtag, final Request request) {
+		String etagSource = DominoUtils.md5(jsonEntity);
+		EntityTag etag = new EntityTag(etagSource);
+		ResponseBuilder berg = null;
+		if (request != null) {
+			berg = request.evaluatePreconditions(etag);
+		}
+		if (berg == null) {
+			berg = Response.ok();
+			if (includeEtag) {
+				berg.tag(etag);
+			}
+			berg.type(MediaType.APPLICATION_JSON_TYPE);
+			berg.entity(jsonEntity);
+			berg.lastModified(lastMod);
+			CacheControl cc = new CacheControl();
+			cc.setMustRevalidate(true);
+			cc.setPrivate(true);
+			cc.setMaxAge(86400);
+			cc.setNoTransform(true);
+			berg.cacheControl(cc);
+		}
+		return berg;
+	}
+
+	private List<?> sortAndLimitList(final List<?> elements, final ParamMap pm) {
+		if (elements instanceof FramedEdgeList) {
+			FramedEdgeList<?> result = (FramedEdgeList<?>) elements;
+			if (pm.getOrderBys() != null) {
+				result = result.sortBy(pm.getOrderBys(), pm.getDescending());
+			}
+			if (pm.getStart() >= 0) {
+				if (pm.getCount() > 0) {
+					result = (FramedEdgeList<?>) result.subList(pm.getStart(), pm.getStart() + pm.getCount());
+				} else {
+					result = (FramedEdgeList<?>) result.subList(pm.getStart(), result.size());
+				}
+			}
+			return result;
+		} else if (elements instanceof FramedVertexList) {
+			FramedVertexList<?> result = (FramedVertexList<?>) elements;
+			if (pm.getOrderBys() != null) {
+				result = result.sortBy(pm.getOrderBys(), pm.getDescending());
+			}
+			if (pm.getStart() >= 0) {
+				if (pm.getCount() > 0) {
+					result = (FramedVertexList<?>) result.subList(pm.getStart(), pm.getStart() + pm.getCount());
+				} else {
+					result = (FramedVertexList<?>) result.subList(pm.getStart(), result.size());
+				}
+			}
+			return result;
+		} else if (elements instanceof MixedFramedVertexList) {
+			MixedFramedVertexList result = (MixedFramedVertexList) elements;
+			if (pm.getOrderBys() != null) {
+				result = result.sortBy(pm.getOrderBys(), pm.getDescending());
+			}
+			if (pm.getStart() > 0) {
+				if (pm.getCount() > 0) {
+					result = (MixedFramedVertexList) result.subList(pm.getStart(), pm.getStart() + pm.getCount());
+				} else {
+					result = (MixedFramedVertexList) result.subList(pm.getStart(), result.size());
+				}
+			}
+			return result;
+		} else
+			throw new IllegalArgumentException(
+					"Cannot sort a list of type " + (elements == null ? "null" : elements.getClass().getName()));
+		// writer.outArrayLiteral(result);
+	}
+
+}
