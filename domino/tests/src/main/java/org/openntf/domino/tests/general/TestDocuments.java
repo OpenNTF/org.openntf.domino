2e582d63c4321d90c372fbeaf27c95e289412b8c
Jesse Gallagher <jesse@secondfoundation.org>
2019-08-12 14:25:45 -0400

    Add test for GitHub issue #55

--- domino/tests/src/main/java/org/openntf/domino/tests/general/TestDocuments.java
+++ domino/tests/src/main/java/org/openntf/domino/tests/general/TestDocuments.java
@@ -1,12 +1,14 @@
 package org.openntf.domino.tests.general;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Vector;
 
 import org.junit.Test;
 import org.openntf.domino.Database;
 import org.openntf.domino.Document;
+import org.openntf.domino.DocumentCollection;
 import org.openntf.domino.Session;
 import org.openntf.domino.tests.AllTests;
 import org.openntf.domino.utils.Factory;
@@ -26,5 +28,29 @@ public class TestDocuments {
 		md.add(session.createDateTime(2010, 3, 1, 12, 0, 0));
         doc.replaceItemValue("fldDate2", md);
         assertEquals(md, doc.getItemValue("fldDate2"));
+	}
+	
+	/**
+	 * Tests for https://github.com/OpenNTF/org.openntf.domino/issues/55
+	 */
+	@Test
+	public void testDocumentCollectionParent() {
+		Session session = Factory.getSession();
+		Database database = session.getDatabase(AllTests.EMPTY_DB);
+		
+		{
+			DocumentCollection docs = database.getAllDocuments();
+			assertTrue("docs parent should be a database", docs.getParent() instanceof Database);
+		}
+		{
+			Document doc = database.createDocument();
+			doc.save();
+			Document child = database.createDocument();
+			child.makeResponse(doc);
+			child.save();
+			
+			DocumentCollection children = doc.getResponses();
+			assertTrue("children parent should be a database", children.getParent() instanceof Database);
+		}
 	}
 }
