38032769934325f3ca103f19f68f0a3f0368140d
Jesse Gallagher <jesse@secondfoundation.org>
2019-08-12 10:01:14 -0400

    Remove errant sysout

--- domino/core/src/main/java/org/openntf/domino/design/impl/DatabaseClassLoader.java
+++ domino/core/src/main/java/org/openntf/domino/design/impl/DatabaseClassLoader.java
@@ -1,16 +1,16 @@
 /*
  * Copyright 2013
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
- * 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, software 
- * distributed under the License is distributed on an "AS IS" BASIS, 
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or 
- * implied. See the License for the specific language governing 
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
  * permissions and limitations under the License.
  */
 
@@ -30,7 +30,7 @@ import org.openntf.domino.utils.DominoUtils;
 
 /**
  * @author jgallagher
- * 
+ *
  */
 public class DatabaseClassLoader extends org.openntf.domino.design.DatabaseClassLoader {
 	@SuppressWarnings("unused")
@@ -109,7 +109,6 @@ public class DatabaseClassLoader extends org.openntf.domino.design.DatabaseClass
 		// Now do the same for Java script libraries
 		if (includeLibraries_ && !loadedLibraries_) {
 			for (org.openntf.domino.design.ScriptLibraryJava lib : design_.getScriptLibrariesJava()) {
-				System.out.println(lib.getName());
 				Map<String, byte[]> classData = lib.getClassData();
 				unloadedClasses_.putAll(classData);
 			}
