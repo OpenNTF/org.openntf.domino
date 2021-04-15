5c2e25d80e2e32c0a0326325259e147ce4636b75
Jesse Gallagher <jesse@secondfoundation.org>
2021-04-15 11:48:26 -0400

    Bump copyright year to 2021

--- domino/core/src/main/java/org/openntf/domino/extmgr/ExtensionManagerBridge.java
+++ domino/core/src/main/java/org/openntf/domino/extmgr/ExtensionManagerBridge.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2013-2020 The OpenNTF Domino API Team
+ * Copyright © 2013-2021 The OpenNTF Domino API Team
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.openntf.domino.extmgr;
 
 import java.lang.reflect.Constructor;
